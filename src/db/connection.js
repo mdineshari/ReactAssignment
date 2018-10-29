b8b1c53da73f4758206f87225765837cabe11444
mdineshari <mdineshari@gmail.com>
2018-10-29 15:33:26 +0530

    heroku connectivity issue
    heroku connectivity issue

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,18 +1,31 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD
-});
+const connection;
 
-connection.connect(function(err) {
-    if(err) {
-        console.error(err);
-        return;
-    }
-
-    console.log('Connected as id ' + connection.threadId);
-});
+function handleDisconnect() {
+    connection = mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        database: process.env.DATABASE_NAME,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD
+    });
+    connection.connect(function(err) {
+        if(err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+            //console.error(err);
+            //return;
+        }
+        connection.on('error', function(err) {
+            console.log('db error', err);
+            if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+              handleDisconnect();                         // lost due to either server restart, or a
+            } else {                                      // connnection idle timeout (the wait_timeout
+              throw err;                                  // server variable configures this)
+            }
+        });
+        console.log('Connected as id ' + connection.threadId);
+    });
+}
+handleDisconnect();
 
 module.exports = connection;
