dec21fd20ee3bc8ea012b79aa450dd69b7b54ddb
mdineshari <mdineshari@gmail.com>
2018-10-29 16:08:40 +0530

    fatal error handler

--- src/db/connection.js
+++ src/db/connection.js
@@ -17,7 +17,7 @@ function handleDisconnect() {
         }
         connection.on('error', function(err) {
             console.log('db error', err);
-            if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            if(err.fatal || err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
               handleDisconnect();                         // lost due to either server restart, or a
             } else {                                      // connnection idle timeout (the wait_timeout
               throw err;                                  // server variable configures this)
